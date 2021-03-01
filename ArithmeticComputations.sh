d323af23e124998dce348dcb27c5e5270b2b9a64
sweetylenka <sweetylenka2013@gmail.com>
2021-03-01 12:39:41 +0530

    Compute a * b + c

--- ArithmeticComputations.sh
+++ ArithmeticComputations.sh
@@ -6,4 +6,10 @@ echo "Enter three numbers :"
 read a 
 read b
 read c
-echo "$a $b $c"
+
+Operation1=$(($a+$b*$c))
+echo "a+b*c :" $Operation1
+
+Operation2=$(($a*$b+$c))
+echo "a*b+c :" $Operation2
+
