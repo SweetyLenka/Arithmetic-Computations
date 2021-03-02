dac93b579a30ec391ce756d5410994a7b9ad13e7
sweetylenka <sweetylenka2013@gmail.com>
2021-03-02 12:22:03 +0530

    Store the results in a Dictionary for every Computation

--- ArithmeticComputations.sh
+++ ArithmeticComputations.sh
@@ -1,1 +1,28 @@
 #WELCOME TO ARITHMETIC COMPUTATIONS
+#!/bin/bash -x
+
+echo "Enter three numbers :"
+read a b c 
+
+Operation1=$(($a+$b*$c))
+echo "a+b*c :" $Operation1
+
+Operation2=$(($a*$b+$c))
+echo "a*b+c :" $Operation2
+
+Operation3=$(($c+$a/$b))
+echo "c+a/b :" $Operation3
+
+Operation4=$(($a%$b+$c))
+echo "a%b+c :" $Operation4
+
+declare -A ArithmeticOperations
+
+ArithmeticOperations[Operation1]=$Operation1
+ArithmeticOperations[Operation2]=$Operation2
+ArithmeticOperations[Operation3]=$Operation3
+ArithmeticOperations[Operation4]=$Operation4
+
+
+echo  "Dictionary : ${ArithmeticOperations[@]}"
+
