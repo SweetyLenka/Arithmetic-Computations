fe212880e72351bde9f8227e12e7f1a25fbde7ec
sweetylenka <sweetylenka2013@gmail.com>
2021-03-02 12:30:16 +0530

    Store the results in a Dictionary for every Computation

--- ArithmeticComputations.sh
+++ ArithmeticComputations.sh
@@ -19,3 +19,13 @@ echo "c+a/b :" $Operation3
 Operation4=$(($a%$b+$c))
 echo "a%b+c :" $Operation4
 
+declare -A ArithmeticOperations
+
+ArithmeticOperations[Operation1]=$Operation1
+ArithmeticOperations[Operation2]=$Operation2
+ArithmeticOperations[Operation3]=$Operation3
+ArithmeticOperations[Operation4]=$Operation4
+
+
+echo  "Dictionary : ${ArithmeticOperations[@]}"
+
